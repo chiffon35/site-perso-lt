21afbc3914f41e4074c020604cea4454046baaa0
OpenShift guest <loic.truchot35@gmail.com>
2013-04-09 12:14:22 -0400

    Express 2

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,22 @@
 /*
  * GET home page.
  */
+var langue = "fr";
+var voc = {
+    et : {
+        fr : "et",
+        en : "and",
+    },
+    parcours : {
+        fr : "Parcours",
+        en: "Parcours"
+    },
+    actualite : {
+        fr : "actualit&eacute;s",
+        en: "actualit&eacute;s"
+    }
+};
 
 exports.index = function(req, res){
-  res.render('index', { title: "Loic TRUCHOT : Parcours et actualit&eacute;s d'un d&eacute;veloppeur web" });
+  res.render('index', { title: "Loic TRUCHOT : " + voc.parcours.fr + " " + voc.en.et + " " + voc.fr.actualite + " d'un d&eacute;veloppeur web" });
 };
