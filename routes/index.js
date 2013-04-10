3684e1c132a8632f22f5500672c4f902eda7877d
OpenShift guest <loic.truchot35@gmail.com>
2013-04-10 10:55:26 -0400

    heroku

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,45 @@
 /*
  * GET home page.
  */
+function PremiereLettreMaj(chaine){
+    return chaine.substr(0,1).toUpperCase()+	chaine.substr(1,chaine.length);		
+}
 var langue = "fr";
 var voc = {
-    et : {
-        fr : "et",
-        en : "and",
+    parcours_et_actualites_d_un_developpeur_web : {
+        fr : "parcours et actualités d'un développeur web",
+        en : "professional path and agenda of a web developer"
     },
-    parcours : {
-        fr : "Parcours",
-        en: "Parcours"
+    accueil : {
+        fr : "accueil",
+        en : "home"
     },
-    actualite : {
-        fr : "actualit&eacute;s",
-        en: "actualit&eacute;s"
+    cv : {
+        fr : "CV",
+        en : "CV"
+    },
+    blog : {
+        fr : "blog",
+        en : "blog"
+    },
+    lttools : {
+        fr : "LTTOoLS",
+        en : "LTTOoLS"
     }
 };
 
+var oEntites = {
+    title : voc.parcours_et_actualites_d_un_developpeur_web.fr,
+    accueil: voc.accueil.fr,
+    cv: voc.cv.fr,
+    blog: voc.cv.fr,
+    lttools: voc.lttools.fr
+};
+for (var sCle in oEntites) {
+    oEntites[sCle] = PremiereLettreMaj(oEntites.sCle);
+}
+
+
 exports.index = function(req, res){
-  res.render('index', { title: "Loic TRUCHOT : " + voc.parcours.fr + " " + voc.et.fr + " " + voc.actualite.fr + " d'un d&eacute;veloppeur web" });
+  res.render('index', oEntites);
 };
