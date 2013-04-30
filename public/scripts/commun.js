cc22a68e879b69807cd62eebc58b75b90a49c579
OpenShift guest <loic.truchot35@gmail.com>
2013-04-30 06:42:39 -0400

    heroku

--- public/scripts/commun.js
+++ public/scripts/commun.js
@@ -1,17 +1,9 @@
-// var tabImagesHover = ["http://site-perso-lt.chiffon35.c9.io/images/bg/bg-recherche-right-hover.png"
-//     , "../images/bg/bg-recherche-right-hover.png"];
-
-// for (var i = 0; i < tabImagesHover.length; i++) {
-//     var image = new Image();
-//     image.src = tabImagesHover[i];
-// }    
-
 //Namespace client
-var SP_LT_CLIENT = SP_LT_CLIENT || {};  
+var SPLT_CLIENT = SPLT_CLIENT || {};  
 
 //Chemins
-SP_LT_CLIENT.PATH = {
-    
+SPLT_CLIENT.PATH = {
+    cheminCourant : window.location.pathname   
 };
 
 //Rendu client
@@ -20,18 +12,11 @@ SP_LT_CLIENT.PATH = {
 //})();
     
 $(document).ready(function () {      
-    preload([
-        '../images/bg/bg-recherche-right-hover.png'
-    ]);
-});
-
-function preload(arrayOfImages) {
-    $(arrayOfImages).each(function(){
-        $('<img/>')[0].src = this;
-        // Alternatively you could use:
-        // (new Image()).src = this;
+    
+    //Boutons de menu pressé ou non
+    $("nav.menu a").each(function () {
+        if ($(this).attr("href") === SPLT_CLIENT.PATH.cheminCourant) {
+            $(this).addClass("elt-menu-pressed");
+        }
     });
-}
-
-// Usage:
-
+});
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,17 @@
  * GET /fr/accueil
  * GET /en/home
  */
+var SPLT_SERVEUR = SPLT_SERVEUR || {};
+SPLT_SERVEUR.setVarJade = function (req) {
+    var jade = {};
+    jade.langue_courante = req.locale;
+    jade.chemin_courant = req.url;
+    return jade;
+};
 exports.index = function(req, res){  
-    res.render('index', {langue_courante : req.locale});
+    res.render('index', SPLT_SERVEUR.setVarJade(req));
 };
 
 exports.blog = function(req, res){
-    res.render('blog', {langue_courante : req.locale})
+    res.render('blog', SPLT_SERVEUR.setVarJade(req))
 };
